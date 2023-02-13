592e19bfc73e15a06e35b69ce517cda8aa556487
afrincodes <afrinsaba7869@gmail.com>
2023-02-13 19:29:22 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,35 @@
+export default [
+    {
+        id: 1,
+        title: 'Srinagar',
+        location: 'Jammu and Kashmir',
+        googleMapsUrl: 'https://www.google.com/maps/place/Srinagar/',
+        startDate: '5 Jan, 2023',
+        endDate: '12 Jan, 2023',
+        description:
+            'The capital city of Srinagar is located 1585 meters above sea level. The city is famous for its gardens, lakes and houseboats. It is also known for traditional Kashmiri handicrafts and dried fruits.',
+        imageUrl: 'https://images.unsplash.com/photo-1607618064912-0559228f8006?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1626&q=80',
+    },
+    {
+        id: 2,
+        title: 'Taj Mahal',
+        location: 'Agra, Uttar Pradesh',
+        googleMapsUrl: 'https://www.google.com/maps/place/Taj+Mahal/@27.1751496,78.0399535,17z/data=!3m1!4b1!4m6!3m5!1s0x39747121d702ff6d:0xdd2ae4803f767dde!8m2!3d27.1751448!4d78.0421422!16zL20vMGw4Y2I',
+        startDate: '27 Sept, 2022',
+        endDate: '3 Oct, 2022',
+        description:
+            "The Taj Mahal is an ivory-white marble mausoleum on the south bank of the Yamuna river in the Indian city of Agra. It was commissioned in 1632 by the Mughal emperor, Shah Jahan (reigned from 1628 to 1658), to house the tomb of his favourite wife, Mumtaz Mahal.",
+        imageUrl: 'https://images.unsplash.com/photo-1585506942812-e72b29cef752?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=428&q=80',
+    },
+    {
+        id: 3,
+        title: 'Jaisalmer Fort',
+        location: 'Rajasthan',
+        googleMapsUrl: 'https://www.google.com/maps/place/Jaisalmer+Fort/@26.9124603,70.9104206,17z/data=!3m1!4b1!4m6!3m5!1s0x3947bd22886171ed:0xd4a507c4923e1973!8m2!3d26.9124555!4d70.9126093!16zL20vMGNrajV6',
+        startDate: '01 Oct, 2021',
+        endDate: '18 Nov, 2021',
+        description:
+            'Jaisalmer Fort is the second oldest fort in Rajasthan, built in 1156 AD by the Rajput Rawal (ruler) Jaisal from whom it derives its name, and stood at the crossroads of important trade routes (including the ancient Silk road).',
+        imageUrl: 'https://images.unsplash.com/photo-1544616751-eea58efccec4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=809&q=80',
+    },
+];
